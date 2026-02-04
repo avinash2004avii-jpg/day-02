52af15478288362f22455cd31870cc7c691d3b47
Avinash <avinash2004avii@gmail.com>
2026-02-04 10:54:56 +0530

    first commit

--- day 3 list and tuples/day03.py
+++ day 3 list and tuples/day03.py
@@ -1,14 +1,53 @@
-# l=[10,20,30,40]
-# print(l)
+l=[10,20,30,40]
+print(l)
 
-# a=[100,200,300,400,500,600,700,800,900]
-# print(a[-3:-1])
-# print(a[1:4:2])
-# print(a[2:9:3])
-# print(a[-7:-2:2])
+a=[100,200,300,400,500,600,700,800,900]
+print(a[-3:-1])
+print(a[1:4:2])
+print(a[2:9:3])
+print(a[-7:-2:2])
 
 l=[2,4,1,9,5,6]
 l.sort()
 print(l)
 l.reverse()
 print(l)
+
+
+#** TASK-1 THE INVENTARY MANAGER
+
+iventary_containing=["apples","banana","carrots","dates"]
+print(iventary_containing)
+
+iventary_containing.append("eggs")
+print(iventary_containing)
+
+iventary_containing.remove("banana")
+print(iventary_containing)
+
+iventary_containing.sort()
+print(iventary_containing)
+
+#** TASK-2 THE DATA SLICER(INDEXING & SLICING)
+
+temperatures=[22,24,25,28,30,29,27,26,24,22]
+print(temperatures[0])
+print(temperatures[-1])
+print(temperatures[3:6])
+print(temperatures[-3:])
+      
+#** TASK-3 THE IMMUTABLE CONFIG
+
+screen_res=(1920,1080)
+print(screen_res)
+#screen_res[0]=1280
+print(screen_res)
+
+
+
+
+
+
+
+
+
